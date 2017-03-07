6fdc3a46b36a8f2fa49b42b76c7bab137c137146
Kelly Davis <kellydavis1974@gmail.com>
2017-03-06 23:52:27 -0500

    testing https: construction for api call (for heroku, which didn't like unsecure call)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ function renderButtons(){
 
 function renderImages(){
 	$("#imagesArea").empty();
-	queryURL = 'http://api.giphy.com/v1/gifs/search?q='+encodeURI($(this).text())+'&limit=10&api_key=dc6zaTOxFJmzC';
+	queryURL = 'https://api.giphy.com/v1/gifs/search?q='+encodeURI($(this).text())+'&limit=10&api_key=dc6zaTOxFJmzC';
 	$.ajax({
     method: "GET",
     url: queryURL,
